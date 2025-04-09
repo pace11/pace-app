67b4ca9e185cd56b46246636b100761b4749e649
pace11 <ryanjoker87@gmail.com>
2025-04-09 23:00:37 +0900

    chore: update docker compose

--- all-script.sh
+++ all-script.sh
@@ -1,4 +1,15 @@
-sudo docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email rppratama1771@gmail.com --agree-tos --no-eff-email -d absensi.pace11.my.id
-
-sudo mkdir -p certbot/www/.well-known/acme-challenge
+# define folder certbot
+sudo mkdir -p certbot/www/.well-known/acme-challenge # primary
 sudo mkdir -p certbot/conf
+
+# run manual generate certbot
+sudo docker run --rm \
+  -v "$(pwd)/certbot/www:/var/www/certbot" \
+  -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
+  certbot/certbot certonly \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  --email email@kamu.com \
+  --agree-tos \
+  --no-eff-email \
+  -d domainkau.com
