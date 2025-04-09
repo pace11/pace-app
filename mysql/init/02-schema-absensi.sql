8592a7b141086cdcdb8556895fe8d7e99b8ff688
pace11 <ryanjoker87@gmail.com>
2025-04-09 23:21:06 +0900

    chore: update docker compose

--- mysql/init/02-schema-absensi.sql
+++ mysql/init/02-schema-absensi.sql
@@ -14,7 +14,7 @@ CREATE TABLE `attendances` (
 	`updated_at` timestamp DEFAULT (now()),
 	CONSTRAINT `attendances_id` PRIMARY KEY(`id`)
 );
---> statement-breakpoint
+
 CREATE TABLE `setting` (
 	`id` int AUTO_INCREMENT NOT NULL,
 	`name` text,
@@ -26,7 +26,7 @@ CREATE TABLE `setting` (
 	`updated_at` timestamp DEFAULT (now()),
 	CONSTRAINT `setting_id` PRIMARY KEY(`id`)
 );
---> statement-breakpoint
+
 CREATE TABLE `users` (
 	`id` int AUTO_INCREMENT NOT NULL,
 	`name` varchar(100) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `users` (
 	CONSTRAINT `users_id` PRIMARY KEY(`id`),
 	CONSTRAINT `users_email_unique` UNIQUE(`email`)
 );
---> statement-breakpoint
+
 ALTER TABLE `attendances` ADD CONSTRAINT `attendances_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE no action ON UPDATE no action;
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'admin', 'admin_db@mail.com', '$2a$12$T4ldlUEycm8gDWOZNUk.0es1Y7y8IkYM1T7Wni4imril8fMtvhcES', 'admin', '2025-04-03 22:52:41', '2025-04-03 22:52:41', NULL);
