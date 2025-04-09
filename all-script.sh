d8f14446050bd4b6c9216aa40989236860acaf12
pace11 <ryanjoker87@gmail.com>
2025-04-09 22:54:33 +0900

    chore: update docker compose

--- all-script.sh
+++ all-script.sh
@@ -1,1 +1,4 @@
-sudo docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email rppratama1771@gmail.com --agree-tos --no-eff-email -d absensi.pace.my.id
+sudo docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email rppratama1771@gmail.com --agree-tos --no-eff-email -d absensi.pace11.my.id
+
+sudo mkdir -p certbot/www/.well-known/acme-challenge
+sudo mkdir -p certbot/conf
